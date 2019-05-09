04c28839c8b08cf5f20f55ee91e470ced6b4d9c2
nivedsanil <nivedsanil@users.noreply.github.com>
2019-05-09 05:00:22 +0400

    carousel-swipe

--- js/main.js
+++ js/main.js
@@ -16,4 +16,13 @@
     });
   }());
 
+  $(document).ready(function() {
+    $("#myCarousel").swiperight(function() {
+       $(this).carousel('prev');
+     });
+    $("#myCarousel").swipeleft(function() {
+       $(this).carousel('next');
+    });
+ });
+
 
